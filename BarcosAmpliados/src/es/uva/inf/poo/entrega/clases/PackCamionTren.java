d31c6e43705edcae5cec8c7cd0d9b8a246a1260f
nachopacho <74068801+nachopacho@users.noreply.github.com>
2021-01-06 19:57:17 +0100

    Update PackCamionTren.java

--- BarcosAmpliados/src/es/uva/inf/poo/entrega/clases/PackCamionTren.java
+++ BarcosAmpliados/src/es/uva/inf/poo/entrega/clases/PackCamionTren.java
@@ -1,11 +1,61 @@
-package es.uva.inf.poo.entrega.clases;
+package es.uva.inf.poo.entraga.clases;
+
+import java.time.LocalDate;
 
 public class PackCamionTren extends Compuesto {
-
-	@Override
-	public double calcularPrecio() {
-		// TODO Auto-generated method stub
-		return 0;
+	
+	/**
+	 * Constante de Coste variable por kilometro de 12,5 euros
+	*/
+	private final double COSTE_KILOMETRO_TREN = 10;
+	
+	/**
+	 * Constante de Coste fijo por trayecto de 400 euros
+	 */
+	private final double COSTE_FIJO_CAMION = 400;
+	/**
+	 * Constante de Coste variable por kilometro de 4,5 euros
+	*/
+	private final double COSTE_KILOMETRO_CAMION = 4.5;
+	
+	
+    public PackCamionTren(Puerto puertoOrigen, Muelle muelleOrigen, Puerto puertoDestino,Muelle muelleDestino, LocalDate fechaSalida, LocalDate fechaLlegada ) {
+		super(puertoOrigen, muelleOrigen, puertoDestino, muelleDestino, fechaSalida, fechaLlegada);
 	}
+	/**
+	 * Anade un nuevo trayecto a la lista de trayectos
+	 * @param trayecto
+	 * @throws IllegalArgumentException en caso de que se intente añadir un trayecto en barco
+	 */
+	public void anadirTrayecto(Simple trayecto) {
+		if (trayecto.getClass().getSimpleName().equals("TBarco")) {
+			throw new IllegalArgumentException();
+		}else {
+			listaTrayectos.add(trayecto);
+		}
+		
+	}
+	/**
+	 * Calcula el precio de todos los trayectos
+	 */
+	
+	
+	
+	public double calcularPrecio() {
+		double suma = 0 ;
+		for (Simple trayecto : listaTrayectos) {
+			if (trayecto.getClass().getSimpleName().equals("TTren")) {
+				suma = suma + ( COSTE_KILOMETRO_TREN*cambiarAKilometros(getMillas()));
+			}else {
+				suma = suma+ (COSTE_FIJO_CAMION+COSTE_KILOMETRO_CAMION*cambiarAKilometros(getMillas()));
+			}
+			
+		}
+		
+		
+		return suma;
+		
+	}
+	
 
 }
