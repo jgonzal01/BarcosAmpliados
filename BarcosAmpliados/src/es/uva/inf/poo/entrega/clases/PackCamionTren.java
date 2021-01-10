2a6f89ecdbe0cda7163a5b0a6f92a663d917d57b
jgonzal01 <74316748+jgonzal01@users.noreply.github.com>
2021-01-10 11:30:47 +0100

    Update PackCamionTren.java
    Nuevo modelo de PackCamionTren donde hay dos trayectos divididos donde el final de uno es el inicio del siguiente

--- BarcosAmpliados/src/es/uva/inf/poo/entrega/clases/PackCamionTren.java
+++ BarcosAmpliados/src/es/uva/inf/poo/entrega/clases/PackCamionTren.java
@@ -1,61 +1,22 @@
-package es.uva.inf.poo.entraga.clases;
+package es.uva.inf.poo.entrega.clases;
 
 import java.time.LocalDate;
 
 public class PackCamionTren extends Compuesto {
-	
-	/**
-	 * Constante de Coste variable por kilometro de 12,5 euros
-	*/
-	private final double COSTE_KILOMETRO_TREN = 10;
-	
-	/**
-	 * Constante de Coste fijo por trayecto de 400 euros
-	 */
-	private final double COSTE_FIJO_CAMION = 400;
-	/**
-	 * Constante de Coste variable por kilometro de 4,5 euros
-	*/
-	private final double COSTE_KILOMETRO_CAMION = 4.5;
-	
-	
-    public PackCamionTren(Puerto puertoOrigen, Muelle muelleOrigen, Puerto puertoDestino,Muelle muelleDestino, LocalDate fechaSalida, LocalDate fechaLlegada ) {
-		super(puertoOrigen, muelleOrigen, puertoDestino, muelleDestino, fechaSalida, fechaLlegada);
-	}
-	/**
-	 * Anade un nuevo trayecto a la lista de trayectos
-	 * @param trayecto
-	 * @throws IllegalArgumentException en caso de que se intente añadir un trayecto en barco
-	 */
-	public void anadirTrayecto(Simple trayecto) {
-		if (trayecto.getClass().getSimpleName().equals("TBarco")) {
-			throw new IllegalArgumentException();
-		}else {
-			listaTrayectos.add(trayecto);
-		}
+
+	public PackCamionTren(Puerto puertoOrigen, Muelle muelleOrigen,Puerto puertoIntermedio,Muelle muelleIntermedio,  Puerto puertoDestino,Muelle muelleDestino, LocalDate fechaSalida, LocalDate fechaLlegada) {
+		super(puertoOrigen,muelleOrigen,puertoIntermedio, muelleIntermedio,  puertoDestino, muelleDestino, fechaSalida, fechaLlegada);
+		
+		t1 = new TCamion(puertoOrigen, muelleOrigen, puertoIntermedio, muelleIntermedio, fechaSalida, fechaLlegada);
+		t2 = new TTren(puertoIntermedio, muelleIntermedio, puertoDestino, muelleDestino, fechaSalida, fechaLlegada);
 		
 	}
-	/**
-	 * Calcula el precio de todos los trayectos
-	 */
-	
-	
 	
 	public double calcularPrecio() {
-		double suma = 0 ;
-		for (Simple trayecto : listaTrayectos) {
-			if (trayecto.getClass().getSimpleName().equals("TTren")) {
-				suma = suma + ( COSTE_KILOMETRO_TREN*cambiarAKilometros(getMillas()));
-			}else {
-				suma = suma+ (COSTE_FIJO_CAMION+COSTE_KILOMETRO_CAMION*cambiarAKilometros(getMillas()));
-			}
-			
-		}
+		Double distT1 = super.cambiarAKilometros(t1.calculaMillas());
+		Double distT2 = super.cambiarAKilometros(t2.calculaMillas());
 		
-		
-		return suma;
-		
+		return 10 * (distT1 + distT2);
 	}
-	
 
 }
