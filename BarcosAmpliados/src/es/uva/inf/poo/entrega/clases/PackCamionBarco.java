926b7daa202c58d78d903cc30d7646f743d87e33
jgonzal01 <74316748+jgonzal01@users.noreply.github.com>
2021-01-10 11:32:44 +0100

    Update PackCamionBarco.java
    Lo mismo que en PackCamionTren

--- BarcosAmpliados/src/es/uva/inf/poo/entrega/clases/PackCamionBarco.java
+++ BarcosAmpliados/src/es/uva/inf/poo/entrega/clases/PackCamionBarco.java
@@ -1,61 +1,17 @@
-package es.uva.inf.poo.entraga.clases;
-
+package es.uva.inf.poo.entrega.clases;
 import java.time.LocalDate;
 
 public class PackCamionBarco extends Compuesto {
 	
-	/**
-	 * Constante de Coste variable por kilometro de 12,5 euros
-	*/
-	private final double COSTE_DIA_BARCO = 4000*0.85;
 	
-	/**
-	 * Constante de Coste fijo por trayecto de 400 euros
-	 */
-	private final double COSTE_FIJO_CAMION = 400;
-	/**
-	 * Constante de Coste variable por kilometro de 4,5 euros
-	*/
-	private final double COSTE_KILOMETRO_CAMION = 4.5;
-	
-	
-    public PackCamionBarco(Puerto puertoOrigen, Muelle muelleOrigen, Puerto puertoDestino,Muelle muelleDestino, LocalDate fechaSalida, LocalDate fechaLlegada ) {
-		super(puertoOrigen, muelleOrigen, puertoDestino, muelleDestino, fechaSalida, fechaLlegada);
-	}
-	/**
-	 * Anade un nuevo trayecto a la lista
-	 * @param trayecto
-	 * @throws IllegalArgumetnException en caso de que se intente meter un trayecto en tren
-	 */
-	public void anadirTrayecto(Simple trayecto) {
-		if (trayecto.getClass().getSimpleName().equals("TTren")) {
-			throw new IllegalArgumentException();
-		}else {
-			listaTrayectos.add(trayecto);
-		}
+	public PackCamionBarco(Puerto puertoOrigen, Muelle muelleOrigen,Puerto puertoIntermedio,Muelle muelleIntermedio,  Puerto puertoDestino,Muelle muelleDestino, LocalDate fechaSalida, LocalDate fechaLlegada) {
+		super(puertoOrigen,muelleOrigen,puertoIntermedio, muelleIntermedio,  puertoDestino, muelleDestino, fechaSalida, fechaLlegada);
 		
+		t1 = new TCamion(puertoOrigen, muelleOrigen, puertoIntermedio, muelleIntermedio, fechaSalida, fechaLlegada);
+		t2 = new TBarco(puertoIntermedio, muelleIntermedio, puertoDestino, muelleDestino, fechaSalida, fechaLlegada);
 	}
-	/**
-	 * Calcula el precio de todos los trayectos 
-	 * @return precio total de los trayectos
-	 */
-	
 	
 	public double calcularPrecio() {
-		double suma = 0 ;
-		for (Simple trayecto : listaTrayectos) {
-			if (trayecto.getClass().getSimpleName().equals("TBarco")) {
-				suma = suma + ( COSTE_DIA_BARCO*calcularDias());
-			}else {
-				suma = suma+ (COSTE_FIJO_CAMION+COSTE_KILOMETRO_CAMION*cambiarAKilometros(getMillas()));
-			}
-			
-		}
-		
-		
-		return suma;
-		
+		return t1.calcularPrecio() + t2.calcularPrecio() * 0.85; 
 	}
-	
-
 }
