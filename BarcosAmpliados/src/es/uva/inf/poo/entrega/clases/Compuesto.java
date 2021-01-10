ced8c78943150f8ea65ebfe31e69fcfbbd29225b
jgonzal01 <74316748+jgonzal01@users.noreply.github.com>
2021-01-10 11:36:22 +0100

    Update Compuesto.java
    He puesto dos trayectos simples protegios en compuesto que heredaran en las siguientes clases hijas de compuesto, serviran para calcular los precio combinados

--- BarcosAmpliados/src/es/uva/inf/poo/entrega/clases/Compuesto.java
+++ BarcosAmpliados/src/es/uva/inf/poo/entrega/clases/Compuesto.java
@@ -1,51 +1,33 @@
-package es.uva.inf.poo.entraga.clases;
-import java.util.ArrayList;
-
+package es.uva.inf.poo.entrega.clases;
 import java.time.LocalDate;
 
 /**
- * Clase que representa una combinacion de varios trayectos. Hereda de la clase Trayecto
+ * Clase que representa un pack de varios trayectos. Hereda de la clase Trayecto
  * @author nacho
  *
  */
-public class Compuesto extends Trayecto {
-	/**
-	 * Lista de trayectos realizados
-	 */
-	protected ArrayList<Simple> listaTrayectos = new ArrayList<Simple>();
-	/**
-	 * Constructor de la clase Compuesto
-	 * @param puertoOrigen
-	 * @param muelleOrigen
-	 * @param puertoDestino
-	 * @param muelleDestino
-	 * @param fechaSalida
-	 * @param fechaLlegada
-	 */
-	public Compuesto(Puerto puertoOrigen, Muelle muelleOrigen, Puerto puertoDestino,Muelle muelleDestino, LocalDate fechaSalida, LocalDate fechaLlegada ) {
-		
+public abstract class Compuesto extends Trayecto {
+	private Muelle muelleIntermedio;
+	private Puerto puertoIntermedio;
+	
+	protected Simple t1;
+	protected Simple t2;
+	
+	public Compuesto(Puerto puertoOrigen, Muelle muelleOrigen,Puerto puertoIntermedio,Muelle muelleIntermedio,  Puerto puertoDestino,Muelle muelleDestino, LocalDate fechaSalida, LocalDate fechaLlegada) {
 		super(puertoOrigen, muelleOrigen, puertoDestino, muelleDestino, fechaSalida, fechaLlegada);
+		this.muelleIntermedio = muelleIntermedio;
+		this.puertoIntermedio = puertoIntermedio;
+		
 	}
-	/**
-	Calcula el precio de la suma de trayectos
-	*/
-	public double calcularPrecio() {
-		double suma=0;
-		for (Simple i : listaTrayectos) {
-			suma = suma + i.calcularPrecio();
-		}
-		return suma;
-	
-	 
+	public double calcularMillasAMuelleIntermedio() {
+		double dist = super.getMuelleOrigen().getGPS().getDistanceTo(muelleIntermedio.getGPS());
+		return dist;
 	}
-	/**
-	 * Añade un trayecto simple a la lista de trayectos
-	 * @param trayecto
-	 */
-	
-	
-	public void añadirTrayecto(Simple trayecto){
-		listaTrayectos.add(trayecto);
+	public Muelle getMuelleIntermedio() {
+		return muelleIntermedio;
 	}
-
+	public Puerto getPuertoIntermedio() {
+		return puertoIntermedio;
+	}
+	
 }
