817bc2d69621e3434239b6bb61caf9356167afba
nachopacho <74068801+nachopacho@users.noreply.github.com>
2021-01-12 22:49:03 +0100

    Update Simple.java

--- BarcosAmpliados/src/es/uva/inf/poo/entrega/clases/Simple.java
+++ BarcosAmpliados/src/es/uva/inf/poo/entrega/clases/Simple.java
@@ -1,16 +1,27 @@
-package es.uva.inf.poo.entrega.clases;
+package es.uva.inf.poo.entraga.clases;
 
 import java.time.LocalDate;
 /**
  * Clase abstracta que representa un trayecto que puede ser por tren por barco o por camion. Hereda de la clase Trayecto
- * @author nacho
+ * @author igndomi 
+ * @author jgonzal
  *
  */
 public abstract class Simple extends Trayecto{
 	
+	/**
+	 * Constructor de la clase Simple
+	 * @param puertoOrigen
+	 * @param muelleOrigen
+	 * @param puertoDestino
+	 * @param muelleDestino
+	 * @param fechaSalida
+	 * @param fechaLlegada
+	 */
 	public Simple(Puerto puertoOrigen, Muelle muelleOrigen, Puerto puertoDestino,Muelle muelleDestino, LocalDate fechaSalida, LocalDate fechaLlegada ) {
 	
 		super(puertoOrigen, muelleOrigen, puertoDestino, muelleDestino, fechaSalida, fechaLlegada);
+		
 	}
 
 }
