0f1ce1d8d6e6b59a5ef08622b8622b06e0db1579
nachopacho <74068801+nachopacho@users.noreply.github.com>
2021-01-12 20:54:36 +0100

    Update FlatRack.java

--- BarcosAmpliados/src/es/uva/inf/poo/entrega/clases/FlatRack.java
+++ BarcosAmpliados/src/es/uva/inf/poo/entrega/clases/FlatRack.java
@@ -1,10 +1,19 @@
-package es.uva.inf.poo.entrega.clases;
+package es.uva.inf.poo.entraga.clases;
 
 public class FlatRack extends Contenedor{
 	
 	public FlatRack(String codDueno, char equip, int numSerie,int digitoControl, double pes, char udPeso, double vol, char udVol,  String est) {
 			super(codDueno,equip,numSerie,digitoControl,pes,udPeso,vol,udVol,est,false);
 	
+	}
+	@Override
+	public void anadirTrayecto(Trayecto tray) {
+		if (tray.getClass().getSimpleName().equals("TCamion")||tray.getClass().getSimpleName().equals("PackCamionTren")||tray.getClass().getSimpleName().equals("PackCamionBarco")) {
+			throw new IllegalArgumentException("No se admiten trayectos en camion");
+			
+		}else {
+			getListaTrayectos().add(tray);
+		}
 	}
 	
 
